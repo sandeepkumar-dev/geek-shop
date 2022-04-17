e2c35c3a7c47f71d0f3a54cd31ec8fe75cd0ed78
Sandeep Kumar <sandeepypb@gmail.com>
2022-04-17 15:52:12 +0530

    refactor: redesign cart page

--- src/components/CartCard/index.jsx
+++ src/components/CartCard/index.jsx
@@ -32,38 +32,50 @@ const CartCard = ({ product }) => {
   }
 
   return (
-    <div className="GsCartCard">
-      <div className="GsCartCard__image">
-        <img src={product_img} alt={product_name} />
+    <div className={`GsCartCard `}>
+      <div className="GsCartCard--container">
+        <div className="GsCartCard__image">
+          <img src={product_img} alt={product_name} />
+        </div>
+        <div className="GsCartCard__info">
+          <div className="GsCartCard__name">
+            <Typography variant="h6">{product_name}</Typography>
+          </div>
+          <div className="GsCartCard__rating">
+            <RatingStars rating={rating} />
+            <Typography variant={"subtitle1"}>{rating_users}</Typography>
+          </div>
+          <div className="GsCartCard__price">
+            <Typography variant="h6">₹{price}</Typography>
+            <Typography variant="subtitle1">₹{originalPrice}</Typography>
+            <Typography variant="subtitle1">({discount}% off)</Typography>
+          </div>
+          <div className="GsCartCard__quantity">
+            <Typography variant="subtitle1">Quantity: </Typography>
+            <button>+</button>
+            <Typography variant="subtitle1">1</Typography>
+            <button>-</button>
+          </div>
+        </div>
       </div>
-      <div className="GsCartCard__info">
-        <div className="GsCartCard__name">
-          <Typography variant="h6">{product_name}</Typography>
-        </div>
-        <div className="GsCartCard__rating">
-          <RatingStars rating={rating} />
-          <Typography variant={"subtitle1"}>{rating_users}</Typography>
-        </div>
-        <div className="GsCartCard__price">
-          <Typography variant="h6">₹{price}</Typography>
-          <Typography variant="subtitle1">₹{originalPrice}</Typography>
-          <Typography variant="subtitle1">({discount}% off)</Typography>
-        </div>
+      <div className="GsCartCard__action--bottom">
+        <Button
+          variant="contained"
+          className="GsPrimaryBtn--light"
+          onClick={MoveToWishList}
+          size="small"
+        >
+          Move to WishList
+        </Button>
+        <Button
+          variant="outlined"
+          color="secondary"
+          size="small"
+          onClick={() => dispatch({ type: "removeFromCart", payload: product.id })}
+        >
+          Remove from Cart
+        </Button>
       </div>
-      <Button
-        variant="contained"
-        className="GsPrimaryBtn--light"
-        onClick={MoveToWishList}
-      >
-        Move to WishList
-      </Button>
-      <Button
-        variant="outlined"
-        color="secondary"
-        onClick={() => dispatch({ type: "removeFromCart", payload: product.id })}
-      >
-        Remove from Cart
-      </Button>
     </div>
   );
 };
